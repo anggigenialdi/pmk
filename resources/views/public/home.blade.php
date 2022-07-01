d5c625cba928e3dc48bc95092fb9b46018617870
Anggi Genialdi <anggigenialdi@gmail.com>
2022-07-01 14:18:36 +0700

    data

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -92,171 +92,171 @@
 
 @section('content')
 
-<div class="container-fluid">
-    <div class="row no-gutters">
-        <div class="col-md-8 offset-md-2">
-            <div class="header">
-                <h4 class="text-light">Data Kasus PMK</h4>
-                <h5 class="text-light">Kota Bandung</h5>
-            </div>
-            <h5>Jumlah Kasus</h5>
-            <div class="row">
-                <!--  -->
-                <div class="col-md-3">
-                    <div class="card bg-danger">
-                        <div class="card-body">
-                            <p class="card-text">Tertular / Terduga</p>
-                            <h5 class="card-title">450</h5>
+    <div class="container-fluid">
+        <div class="row no-gutters">
+            <div class="col-md-8 offset-md-2">
+                <div class="header">
+                    <h4 class="text-light">Data Kasus PMK</h4>
+                    <h5 class="text-light">Kota Bandung</h5>
+                </div>
+                <h5>Jumlah Kasus</h5>
+                <div class="row">
+                    <!--  -->
+                    <div class="col-md-3">
+                        <div class="card bg-danger">
+                            <div class="card-body">
+                                <p class="card-text">Tertular / Terduga</p>
+                                <h5 class="card-title" id="grand_total">0</h5>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <!--  -->
-                <!--  -->
-                <div class="col-md-3">
-                    <div class="card">
-                        <div class="card-body bg-success">
-                            <p class="card-text">Sembuh</p>
-                            <h5 class="card-title">120</h5>
+                    <!--  -->
+                    <!--  -->
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-body bg-success">
+                                <p class="card-text">Sembuh</p>
+                                <h5 class="card-title" id="sembuh">0</h5>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <!--  -->
-                <!--  -->
-                <div class="col-md-3">
-                    <div class="card">
-                        <div class="card-body bg-warning">
-                            <p class="card-text">Mati</p>
-                            <h5 class="card-title">100</h5>
+                    <!--  -->
+                    <!--  -->
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-body bg-warning">
+                                <p class="card-text">Mati</p>
+                                <h5 class="card-title" id="mati">0</h5>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <!--  -->
-                <!--  -->
-                <div class="col-md-3">
-                    <div class="card">
-                        <div class="card-body bg-primary">
-                            <p class="card-text">Potong Bersyarat</p>
-                            <h5 class="card-title">50</h5>
+                    <!--  -->
+                    <!--  -->
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-body bg-primary">
+                                <p class="card-text">Potong Bersyarat</p>
+                                <h5 class="card-title" id="potong_bersyarat">0</h5>
+                            </div>
                         </div>
                     </div>
+                    <!--  -->
                 </div>
-                <!--  -->
-            </div>
-            <p class="text-right">Update Terakhir : 20 Juni 2022</p>
+                <p class="text-right">Update Terakhir : 20 Juni 2022</p>
 
-        </div>
-    </div>
-    <div class="row no-gutters">
-        <div class="col-md-2">
-        </div>
-        <div class="col-md-8">
-            <h5 class="mt-4 mb-2">Sebaran Kasus PMK di Kota Bandung</h5>
-            <div id="map">
-                <div id="table-kecamatan"></div>
-                <!-- <div id="keterangan-jumlah">
-              <div class="max">
-                <div></div>
-                <div>>1.000 Kasus</div>
-              </div>
-              <div class="middle">
-                <div></div>
-                <div>>500 Kasus</div>
-              </div>
-              <div class="low">
-                <div></div>
-                <div>>100 Kasus</div>
-              </div>
-            </div> -->
             </div>
         </div>
-        <div class="col-md-2">
+        <div class="row no-gutters">
+            <div class="col-md-2">
+            </div>
+            <div class="col-md-8">
+                <h5 class="mt-4 mb-2">Sebaran Kasus PMK di Kota Bandung</h5>
+                <div id="map">
+                    <div id="table-kecamatan"></div>
+                    <!-- <div id="keterangan-jumlah">
+                      <div class="max">
+                        <div></div>
+                        <div>>1.000 Kasus</div>
+                      </div>
+                      <div class="middle">
+                        <div></div>
+                        <div>>500 Kasus</div>
+                      </div>
+                      <div class="low">
+                        <div></div>
+                        <div>>100 Kasus</div>
+                      </div>
+                    </div> -->
+                </div>
+            </div>
+            <div class="col-md-2">
+            </div>
         </div>
     </div>
-</div>
 
 @endsection
 @section('script')
-<script>
-    $(document).ready(function() {
-        setMap()
-        setJumlah()
-    })
-
-    function setJumlah(id) {
-        $.ajax({
-            type: "GET",
-            url: `../data-ternak`,
-            dataType: 'json',
-            async: false,
-            success: function(res) {
-                // res.forEach(element => {
-                //   console.log(element.data_ternak[0].total_kasus)
-                // });
-                //     return String(res.data_pmk[0].total_kasus)
-                $(`#data-${id}`).html(`Total Kasus : ${res.data_pmk[0].total_kasus}`);
-            }
+    <script>
+        $(document).ready(function() {
+            setMap()
+            setJumlah()
         })
 
-    }
-
-    function setMap() {
-        var map = L.map('map').setView([-6.904744, 107.639810], 13);
-        var tiles = L.tileLayer(
-            'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-                maxZoom: 18,
-                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                id: 'mapbox/streets-v11',
-                tileSize: 512,
-                zoomOffset: -1
-            }).addTo(map);
-        $.get('../geojson', function(data) {
-            let features = data.features
-            features.forEach((e, index) => {
-                L.geoJSON(e).setStyle({
-                    fillColor: 'blue'
-                }).addTo(map).on('mouseover', () => {
-                    setData(e.properties.id_kecamatan)
-                }).bindTooltip(
-                    '<div>' + e.properties.nama_kecamatan + '</div><div id="data-' + e.properties
-                    .id_kecamatan + '"></div>', {
-                        permanent: true,
-                        direction: "center",
-                        className: "my-labels"
-                    }).openTooltip();
-                // setJumlah(e.properties.id_kecamatan)
-            });
-
-        });
-    }
-
-    function setColor(id) {
-        $.ajax({
-            type: "GET",
-            url: `../data-ternak/${id}`,
-            dataType: 'json',
-            async: false,
-            success: function(res) {
-                if (res.data_pmk[0].total_kasus == '15') {
-                    console.log(res.data_pmk[0].total_kasus)
-                    return '#ffff'
-                } else {
-                    console.log(res.data_pmk[0].total_kasus)
-                    return '#0000'
+        function setJumlah(id) {
+            $.ajax({
+                type: "GET",
+                url: `../data-ternak`,
+                dataType: 'json',
+                async: false,
+                success: function(res) {
+                    // res.forEach(element => {
+                    //   console.log(element.data_ternak[0].total_kasus)
+                    // });
+                    //     return String(res.data_pmk[0].total_kasus)
+                    $(`#data-${id}`).html(`Total Kasus : ${res.data_pmk[0].total_kasus}`);
                 }
-            }
-        })
+            })
 
-    }
+        }
 
-    function setData(id) {
-        $.ajax({
-            type: "GET",
-            url: `../data-ternak/${id}`,
-            dataType: 'json',
-            async: false,
-            success: function(res) {
-                let data = `
+        function setMap() {
+            var map = L.map('map').setView([-6.904744, 107.639810], 13);
+            var tiles = L.tileLayer(
+                'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                    maxZoom: 18,
+                    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+                        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                    id: 'mapbox/streets-v11',
+                    tileSize: 512,
+                    zoomOffset: -1
+                }).addTo(map);
+            $.get('../geojson', function(data) {
+                let features = data.features
+                features.forEach((e, index) => {
+                    L.geoJSON(e).setStyle({
+                        fillColor: 'blue'
+                    }).addTo(map).on('mouseover', () => {
+                        setData(e.properties.id_kecamatan)
+                    }).bindTooltip(
+                        '<div>' + e.properties.nama_kecamatan + '</div><div id="data-' + e.properties
+                        .id_kecamatan + '"></div>', {
+                            permanent: true,
+                            direction: "center",
+                            className: "my-labels"
+                        }).openTooltip();
+                    // setJumlah(e.properties.id_kecamatan)
+                });
+
+            });
+        }
+
+        function setColor(id) {
+            $.ajax({
+                type: "GET",
+                url: `../data-ternak/${id}`,
+                dataType: 'json',
+                async: false,
+                success: function(res) {
+                    if (res.data_pmk[0].total_kasus == '15') {
+                        console.log(res.data_pmk[0].total_kasus)
+                        return '#ffff'
+                    } else {
+                        console.log(res.data_pmk[0].total_kasus)
+                        return '#0000'
+                    }
+                }
+            })
+
+        }
+
+        function setData(id) {
+            $.ajax({
+                type: "GET",
+                url: `../data-ternak/${id}`,
+                dataType: 'json',
+                async: false,
+                success: function(res) {
+                    let data = `
                   <p style="font-size:14px" class="text-center">Data PMK Kecamatan ${res.nama_kecamatan.nama}</p>
                   <table class="table table-striped" style="height:10px;font-size:14px">
                               <thead>
@@ -267,20 +267,56 @@
                                 </tr>
                               </thead>
                               <tbody>`;
-                res.data_pmk_perkelurahan.forEach((res, index) => {
-                    data += `
+                    res.data_pmk_perkelurahan.forEach((res, index) => {
+                        data += `
                                       <tr>
                                         <th scope="row">${index+1}</th>
                                         <td>${res.nama_kelurahan}</td>
                                         <td>${res.ternak[0].total_kasus}</td>
                                       </tr>
                                     `;
-                })
-                data += `</tbody>
+                    })
+                    data += `</tbody>
                             </table>`
-                $('#table-kecamatan').html(data);
-            }
+                    $('#table-kecamatan').html(data);
+                }
+            })
+        }
+        $(document).ready(function() {
+            $.ajax({
+                type: "GET",
+                url: '../data-ternak',
+                dataType: 'json',
+                async: false,
+                success: function(res) {
+                    let gt = 0;
+                    let sembuh = 0;
+                    let potong_bersyarat = 0;
+                    let mati = 0;
+
+                    res.forEach((el, index) => {
+
+                        let sum_terduga = (el.data_ternak.terduga_kambing + el.data_ternak
+                            .terduga_kerbau + el.data_ternak.terduga_sapi_perah + el
+                            .data_ternak.terduga_sapi_potong);
+                        let sum_tertular = (el.data_ternak.tertular_kambing + el.data_ternak
+                            .tertular_kerbau + el.data_ternak.tertular_sapi_perah + el
+                            .data_ternak.tertular_sapi_potong);
+                        gt = gt + (sum_terduga + sum_tertular);
+                        sembuh = sembuh + el.data_ternak.sembuh;
+                        potong_bersyarat = potong_bersyarat + el.data_ternak.potong_bersyarat;
+                        mati = mati + el.data_ternak.mati;
+
+                    })
+                    $('#grand_total').text(`${gt}`)
+                    $('#sembuh').text(`${sembuh}`)
+                    $('#potong_bersyarat').text(`${potong_bersyarat}`)
+                    $('#mati').text(`${mati}`)
+                    
+
+
+                }
+            })
         })
-    }
-</script>
+    </script>
 @stop
